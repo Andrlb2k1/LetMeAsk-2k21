7bb64d8285df5260e27efe8af07a2bfc6b6615b2
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2021-10-15 00:22:39 -0300

    Add files via upload

---
+++ letmeaskpj/src/contexts/AuthContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import firebase from "../services/firebase";
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+export const AuthContext = createContext({} as AuthContextType);
+export function AuthContextProvider(props: AuthContextProviderProps) {
+  const [user, setUser] = useState<User>();
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.');
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+  function signInWithGoogle():any{
+    console.log('chamou auth com google');
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      
+      console.log(result);
+      if(result.user) {
+        if(!result.user.displayName || !result.user.photoURL) {
+          throw new Error('Missing information from Google Account!');
+        }
+        setUser({
+          id: result.user.uid,
+          name: result.user.displayName,
+          avatar: result.user.photoURL
+        })
+      }
+    });
+    
+  }
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
---
+++ letmeaskpj/src/services/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyCOKD6BuXQauOx6iM9zlXhe5dtpg3hoU88",
+  authDomain: "letmeask2k21-218cc.firebaseapp.com",
+  databaseURL: "https://letmeask2k21-218cc-default-rtdb.firebaseio.com",
+  projectId: "letmeask2k21-218cc",
+  storageBucket: "letmeask2k21-218cc.appspot.com",
+  messagingSenderId: "30544392737",
+  appId: "1:30544392737:web:8017bf38f13b7f8150e960",
+  measurementId: "G-8CR4REKMWR"
+};
+firebase.initializeApp(firebaseConfig)
+export default firebase;
